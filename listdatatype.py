145c29e4f7de6c045850b7eb02b7feb9ce0c2782
gopi <vgkrishna332@gmail.com>
2026-04-28 20:19:27 +0530

    feature branch update from gitstuff branch

--- listdatatype.py
+++ listdatatype.py
@@ -2,3 +2,14 @@ emp1 = [101, "gopi", "ste", 35]
 
 emp1.append(75000)
 print(emp1)
+
+emp1[1] = "Krishna"
+print(emp1)
+
+emp1.reverse()
+print(emp1)
+
+emp2 = [102, "gopi", "sdet", 35, 80000]
+print(emp2)
+reversed_emp2 = list(reversed(emp2))
+print(reversed_emp2)
