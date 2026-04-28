3cc3896f522f8e7ad464f57006e3ffae1ba1f48e
gopi <vgkrishna332@gmail.com>
2026-04-28 17:08:48 +0530

    first commit

---
+++ ploops.py
@@ -0,0 +1,23 @@
+x = 10
+
+while x > 1:
+    if x == 9:
+        x = x-1
+        continue
+    if x == 3:
+        break
+    print(x)
+    x = x - 1
+
+print("------------------------")
+
+time = 11.5
+
+if 5<= time <=11:
+    print("Good Morning")
+elif 11< time <=17:
+    print("Good Afternoon")
+elif 17< time <=23:
+    print("Good Evening")
+else:
+    print("Good Night")
